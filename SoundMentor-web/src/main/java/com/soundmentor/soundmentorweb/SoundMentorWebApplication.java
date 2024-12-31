0a2cc69a4b562278b88bb9e6a33ca0844a6f146c
lzc <291396312@qq.com>
2024-12-31 16:58:02 +0800

    基础项目模块结构初始化

---
+++ SoundMentor-web/src/main/java/com/soundmentor/soundmentorweb/SoundMentorWebApplication.java
@@ -0,0 +1,13 @@
+package com.soundmentor.soundmentorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundMentorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundMentorWebApplication.class, args);
+    }
+
+}
